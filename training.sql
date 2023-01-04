da1674df02b27ca07aa7c3d8f0b34f19fd1afb39
AlirezaAliakbari <aliresa021@gmail.com>
2023-01-04 13:49:59 +0330

    first file

---
+++ training.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE record_company;
+USE record_company;
+CREATE TABLE bands (
+	id INT NOT NULL AUTO_INCREMENT,
+	namePRIMARYPRIMARY VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+CREATE TABLE albums (
+	id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(255) NOT NULL,
+    release_year INT,
+    band_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (band_id) REFERENCES bands(id)
+    );
+
