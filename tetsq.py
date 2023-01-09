dba9376117f5a8686a7c6f10c6ee08f3e02df1bc
Alireza Aliakbari <115478245+AlirezaAliakbari@users.noreply.github.com>
2023-01-09 13:05:15 +0330

    Add files via upload

--- tetsq.py
+++ tetsq.py
@@ -2,6 +2,7 @@ from tkinter import *
 #from PIL import ImageTk,Image
 import mysql.connector
 import csv
+from tkinter import ttk
 
 root = Tk()
 root.title("arvin")
@@ -88,10 +89,10 @@ def add_customer():
     clear_fields()
 # write to CSV Excel Function
 def write_to_csv(result):
-    with open("customer.csv", "a", newline="") as f:
+    with open("customer.csv", "a",newline = "") as f:
         w = csv.writer(f, dialect= "excel")
-        for records in result:
-            w.writerow(records)
+        for record in result:
+            w.writerow(record)
 
 # search customers
 def search_customer():
@@ -99,18 +100,49 @@ def search_customer():
     search_customers.title("Search All Customers")
     search_customers.geometry("800x600") 
     def search_now():
+        selected = drop.get()
+        sql = ""
+        if selected == "Search by...":
+            test = Label(search_customers, text = "you picked searched by")
+            test.grid(row=2 , column = 0)
+        if selected == "Last Name":
+            sql = "SELECT * FROM customers WHERE last_name = %s"
+            test = Label(search_customers, text = "you picked Last name")
+            test.grid(row=2 , column = 0)
+        if selected == "Email Address":
+            sql = "SELECT * FROM customers WHERE email = %s"
+            test = Label(search_customers, text = "you picked Email Address")
+            test.grid(row=2 , column = 0)
+        if selected == "Customer ID":
+            sql = "SELECT * FROM customers WHERE user_id = %s"
+            test = Label(search_customers, text = "you picked Customer ID")
+            test.grid(row=2 , column = 0)   
+
+        
         searched = search_box.get()
-        sql = "SELECT * FROM customers WHERE last_name = %s"
+        # sql = "SELECT * FROM customers WHERE last_name = %s"
         name = (searched, )
         result = my_cursor.execute(sql, name)
         result = my_cursor.fetchall()
 
         if not result:
             result = "Record Not Found..."
+            searched_label = Label(search_customers, text =result)
+            searched_label.grid(row=2 , column=0)
+        else:
+            for index, x in enumerate(result):
+                 num = 0
+                 index += 3
+                 for y in x:
+                        searched_label = Label(search_customers, text =y)
+                        searched_label.grid(row=index , column=num)
+                        num += 1
+            csv_button = Button(search_customers, text = "Save to Excel", command= lambda: write_to_csv(result))
+            csv_button.grid(row=index+1, column=0)
 
-        searched_label =Label(search_customers, text=result)
-        searched_label.grid(row=2, column=0, padx=10, columnspan=2)
-
+        #searched_label =Label(search_customers, text=result)
+        #searched_label.grid(row=3, column=0, padx=10, columnspan=2)
+        
     # Entry box to search for customers
     search_box = Entry(search_customers)
     search_box.grid(row=0 , column=1, padx=10, pady=10)
@@ -120,6 +152,11 @@ def search_customer():
     # search Button for customer
     search_button =Button(search_customers, text="Search Customers", command=search_now)
     search_button.grid(row=1, column=0, padx = 10)
+    # Drop Down Box
+    drop = ttk.Combobox(search_customers, value=["Search by...", "Last Name", "Email Address", "Customer ID" ])
+    drop.current(0)
+    drop.grid(row = 0 , column=2)
+
 #list customers
 def list_customers():
     list_customer_query = Tk()
